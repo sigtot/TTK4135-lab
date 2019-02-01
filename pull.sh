46cdcea6e3bce7636eb66c9e9d7a91ce062b1da6
sigtot <sigurdtotland@gmail.com>
2019-02-01 11:05:20 +0100

    fr. 01. feb. 11:05:20 +0100 2019

--- pull.sh
+++ pull.sh
@@ -2,6 +2,6 @@
 shopt -s extglob
 rm -r !(pull.sh|.git|.|..)
 
-scp -r ntnu:/home/shomec/s/sigurdvt/Documents/opt_reg/ .
+scp -r ntnu:/home/shomec/s/sigurdvt/Documents/opt_reg/* .
 git add -A
 git commit -m "`date`"
